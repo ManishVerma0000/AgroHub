0e0713e127f6d5ee998928bb2ecf2a86af798553
ManishVerma0000 <manishverma3224@gmail.com>
2026-07-11 19:47:27 +0530

    added the code for the master

--- services/api.ts
+++ services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? 'http://127.0.0.1:8000/api/v1' 
-    : 'https://api.healthvandanam.com/api/v1',
+    : 'https://apiprod.dvmsolution.co.in/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
